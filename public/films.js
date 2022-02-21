ce5f2aaf4837ba9a0872a662d66a5fcfd5e3f7f2
Kostich31 <k.v.kostinich@mail.ru>
2022-02-21 16:00:32 +0300

    Изменил структуру контейнера для фильма

--- public/films.js
+++ public/films.js
@@ -1,43 +1,83 @@
-
 import { menuPage } from './menu.js';
 
 export function filmsPage() {
 	root.innerHTML = '';    
     menuPage();
 
+    // Контейнер для всех фильмов
     const filmsContainer = document.createElement("div");
     filmsContainer.classList.add("container-films");
 
+    //Заголовок для страницы
     const pageLabel = document.createElement("h1");
     pageLabel.innerHTML = "<center>Топ 250</center>";
     root.appendChild(pageLabel);
 
+    // Контейнер для фильма
     const film = document.createElement("div");
     film.classList.add("film");
     filmsContainer.appendChild(film);
 
+    //Порядковый номер фильма
     const filmNumber = document.createElement("div");
     filmNumber.classList.add("filmNumber");
     filmNumber.innerHTML = "<b>1</b>";
     film.appendChild(filmNumber);
 
+    // Изображение фильма
     const filmImage = document.createElement("div");
     filmImage.classList.add("filmImage");
     filmImage.innerHTML = `<img src="iron-man-new-5k-2w-1024x768.jpg" width="200"/>`
     film.appendChild(filmImage);
 
-    const filmName = document.createElement("div");
-    filmName.classList.add("filmName");
-    filmName.innerHTML = "FilmName";
-    film.appendChild(filmName);
+    //Тело фильма
+    const film_body = document.createElement("div");
+    film_body.classList.add("film_body");
 
+    // Краткое описание фильма
+    const filmBriefDescription = document.createElement("div");
+    filmBriefDescription.classList.add("filmBriefDescription");
+    
+    // Ссылка на страницу фильма
+    const filmOverviewLink = document.createElement("a");
+    filmOverviewLink.classList.add("filmOverviewLink");
+    filmBriefDescription.appendChild(filmOverviewLink);
+
+    // Название фильма на русском
+    const filmOverviewName = document.createElement("p");
+    filmOverviewName.classList.add("filmOverviewName");
+    filmOverviewName.innerHTML = "Железный человек";
+
+    // Название фильма на оригинальном языке
+    const filmOverviewNameOriginal = document.createElement("p");
+    filmOverviewNameOriginal.classList.add("filmOverviewNameOriginal");
+    filmOverviewNameOriginal.innerHTML = "Iron man, 2007";
+
+    // Дополнительная информация о фильме(страна, жанры)
+    const filmOverviewAdditional = document.createElement("p");
+    filmOverviewAdditional.classList.add("filmOverviewAdditional");
+    filmOverviewAdditional.innerHTML = "США * Боевик * Драма";
+
+    filmOverviewLink.appendChild(filmOverviewName);
+    filmOverviewLink.appendChild(filmOverviewNameOriginal);
+    filmOverviewLink.appendChild(filmOverviewAdditional);
+
+    // Краткое содержание фильма
     const filmDescription = document.createElement("div");
     filmDescription.classList.add("filmDescription");
+    filmDescription.innerHTML = "Попав в плен, Тони Старк изобретает суперкостюм и спасает мир. Блокбастер, запустивший киновселенную Marvel";
 
-    filmDescription.innerHTML = "Film Desckr";
-    film.appendChild(filmDescription);
+    // Оценка фильма
+    const filmRating = document.createElement("div")
+    filmRating.classList.add("filmRating");
+    filmRating.innerHTML = "8.5";
+
+    film_body.appendChild(filmBriefDescription);
+    film_body.appendChild(filmDescription);
+    film_body.appendChild(filmRating);
 
 
+    film.appendChild(film_body);
     //
 
     const film2 = document.createElement("div");
@@ -46,7 +86,7 @@ export function filmsPage() {
 
     const filmNumber2 = document.createElement("div");
     filmNumber2.classList.add("filmNumber");
-    filmNumber2.innerHTML = "<b>1</b>";
+    filmNumber2.innerHTML = "<b>2</b>";
     film2.appendChild(filmNumber2);
 
     const filmImage2 = document.createElement("div");
