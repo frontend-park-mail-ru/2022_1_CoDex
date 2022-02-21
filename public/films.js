bc89cd5e5b5518d9e8cae42429352b7009a89405
Kostich31 <k.v.kostinich@mail.ru>
2022-02-21 17:12:57 +0300

    Добавил второй элемент

--- public/films.js
+++ public/films.js
@@ -78,32 +78,74 @@ export function filmsPage() {
 
 
     film.appendChild(film_body);
-    //
 
+    ////////////////////////////////////
+
+    // Контейнер для фильма
     const film2 = document.createElement("div");
     film2.classList.add("film");
     filmsContainer.appendChild(film2);
 
+    //Порядковый номер фильма
     const filmNumber2 = document.createElement("div");
     filmNumber2.classList.add("filmNumber");
     filmNumber2.innerHTML = "<b>2</b>";
     film2.appendChild(filmNumber2);
 
+    // Изображение фильма
     const filmImage2 = document.createElement("div");
     filmImage2.classList.add("filmImage");
-    filmImage2.innerHTML = `<img src="149219-mstiteli-art-supergeroj-bronya-cifrovoe_iskusstvo-1920x1080.jpg" width="200"/>`
+    filmImage2.innerHTML = `<img src="iron-man-new-5k-2w-1024x768.jpg" width="200"/>`
     film2.appendChild(filmImage2);
 
-    const filmName2 = document.createElement("div");
-    filmName2.classList.add("filmName");
-    filmName2.innerHTML = "filmName2";
-    film2.appendChild(filmName2);
+    //Тело фильма
+    const film_body2 = document.createElement("div");
+    film_body2.classList.add("film_body");
 
+    // Краткое описание фильма
+    const filmBriefDescription2 = document.createElement("div");
+    filmBriefDescription2.classList.add("filmBriefDescription");
+    
+    // Ссылка на страницу фильма
+    const filmOverviewLink2 = document.createElement("a");
+    filmOverviewLink2.classList.add("filmOverviewLink");
+    filmBriefDescription2.appendChild(filmOverviewLink2);
+
+    // Название фильма на русском
+    const filmOverviewName2 = document.createElement("p");
+    filmOverviewName2.classList.add("filmOverviewName");
+    filmOverviewName2.innerHTML = "Железный человек";
+
+    // Название фильма на оригинальном языке
+    const filmOverviewNameOriginal2 = document.createElement("p");
+    filmOverviewNameOriginal2.classList.add("filmOverviewNameOriginal");
+    filmOverviewNameOriginal2.innerHTML = "Iron man, 2007";
+
+    // Дополнительная информация о фильме(страна, жанры)
+    const filmOverviewAdditional2 = document.createElement("p");
+    filmOverviewAdditional2.classList.add("filmOverviewAdditional");
+    filmOverviewAdditional2.innerHTML = "США * Боевик * Драма";
+
+    filmOverviewLink2.appendChild(filmOverviewName2);
+    filmOverviewLink2.appendChild(filmOverviewNameOriginal2);
+    filmOverviewLink2.appendChild(filmOverviewAdditional2);
+
+    // Краткое содержание фильма
     const filmDescription2 = document.createElement("div");
     filmDescription2.classList.add("filmDescription");
+    filmDescription2.innerHTML = "Попав в плен, Тони Старк изобретает суперкостюм и спасает мир. Блокбастер, запустивший киновселенную Marvel";
 
-    filmDescription2.innerHTML = "Film Desckr";
-    film2.appendChild(filmDescription2);
+    // Оценка фильма
+    const filmRating2 = document.createElement("div")
+    filmRating2.classList.add("filmRating");
+    filmRating2.innerHTML = "8.5";
+
+    film_body2.appendChild(filmBriefDescription2);
+    film_body2.appendChild(filmDescription2);
+    film_body2.appendChild(filmRating2);
+
+
+    film2.appendChild(film_body2);
 
 
 	root.appendChild(filmsContainer);
