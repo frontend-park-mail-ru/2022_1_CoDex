269a15e8e4bfa14b725f880a5a6f9bc4e52668dd
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-02-28 23:00:25 +0300

    Signup form done

--- public/signup.js
+++ public/signup.js
@@ -1,4 +1,6 @@
 import { ajax } from './main.js';
+import { clearContent } from './menu.js';
+
 
 export function createInput(type, text, name) {
     const input = document.createElement('input');
@@ -10,98 +12,94 @@ export function createInput(type, text, name) {
 }
 
 export function signupPage(warning = 0) {
-    root.innerHTML = '';
+    const content = clearContent();
+
+    const signupContent = document.createElement("div");
+    signupContent.classList.add("signup-content");
     const div = document.createElement("div");
-    div.classList.add("container");
+    div.classList.add("signup");
 
-    const backForm = document.createElement('form');
-    backForm.action = "http://0.0.0.0:3000";
-    div.appendChild(backForm);
-
-    const backBtn = document.createElement("Button");
-    backBtn.classList.add("back");
-    backBtn.type = "submit";
-    backBtn.innerHTML = "🠈 Back";
-    backForm.appendChild(backBtn);
-
-    const pageLabel = document.createElement("h1");
-    pageLabel.innerHTML = "<center>Регистрация</center>";
-    div.appendChild(pageLabel);
-
-    const descriptionLabel = document.createElement("p");
-    descriptionLabel.innerHTML = "Пожалуйста, заполните эту форму, чтобы создать аккаунт";
-    div.appendChild(descriptionLabel);
-
-    const h = document.createElement("hr");
-    div.appendChild(h);
+    const signupInvitation = document.createElement("div");
+    signupInvitation.classList.add("signup__invitation");
+    signupInvitation.textContent = "Зарегистрироваться";
+    div.appendChild(signupInvitation);
 
     const form = document.createElement('form');
-    h.appendChild(form);
+    form.classList.add("signup__form");
+    div.appendChild(form);
 
     const emailLabel = document.createElement("Label");
-    emailLabel.innerHTML = "<b>E-mail</b>";
+    emailLabel.textContent = "Email";
+    emailLabel.classList.add("text_inputs__label");
     emailLabel.setAttribute("for", "email");
 
     const emailInput = createInput('email', 'Введите e-mail', 'email');
+    emailInput.classList.add("text_inputs");
     emailInput.name = "email";
-    emailInput.type = "text";
-
-    const passwordLabel = document.createElement("Label");
-    passwordLabel.innerHTML = "<b>Пароль</b>";
-    passwordLabel.setAttribute("for", "password");
-    const passwordInput = createInput('password', 'Введите пароль', 'password');
-
-    const repeatePasswordLabel = document.createElement("Label");
-    repeatePasswordLabel.innerHTML = "<b>Повторите пароль</b>";
-    repeatePasswordLabel.setAttribute("for", "repeatePassword");
-    const repeatePasswordInput = createInput('password', 'Повторите пароль', 'repeatePassword');
-
-    let warningLabel;
-    if (warning == 1) {
-        warningLabel = document.createElement("Label");
-        warningLabel.classList.add("warning");
-        warningLabel.innerHTML = "<p>Passwords are different</p>";
-    } else if (warning == 2) {
-        warningLabel = document.createElement("Label");
-        warningLabel.classList.add("warning");
-        warningLabel.innerHTML = "<p>User exists</p>";
-    }
-
-
-    const terms = document.createElement("p");
-    terms.innerHTML = "Создавая аккаунт, Вы соглашаетесь с <a href=\"#\">Правилами и условиями</a>.";
-    
-
-    const submitBtn = document.createElement('Button');
-    submitBtn.type = 'submit';
-    submitBtn.classList.add("registerbtn");
-    submitBtn.innerHTML = 'Зарегистрироваться';
+    emailInput.type = "email";
 
     form.appendChild(emailLabel);
     form.appendChild(emailInput);
+
+    
+    const passwordLabel = document.createElement("Label");
+    passwordLabel.classList.add("text_inputs__label");
+    passwordLabel.textContent = "Пароль";
+    passwordLabel.setAttribute("for", "password");
+    const passwordInput = createInput('password', 'Введите пароль', 'password');
+    passwordInput.classList.add("text_inputs");
+    
     form.appendChild(passwordLabel);
     form.appendChild(passwordInput);
-    form.appendChild(repeatePasswordLabel);
-    form.appendChild(repeatePasswordInput);
-    if (warning != 0) {
-        form.appendChild(warningLabel); 
-    }
-    form.appendChild(terms);
-    form.appendChild(submitBtn);
+    
+    const repeatPasswordLabel = document.createElement("Label");
+    repeatPasswordLabel.classList.add("text_inputs__label");
+    repeatPasswordLabel.textContent = "Повторите пароль";
+    repeatPasswordLabel.setAttribute("for", "repeatPassword");
+    const repeatPasswordInput = createInput('password', 'Введите пароль', 'repeatPassword');
+    repeatPasswordInput.classList.add("text_inputs");
+    form.appendChild(repeatPasswordLabel);
+    form.appendChild(repeatPasswordInput);
+    const loginButton = document.createElement("input");
+    loginButton.type = "button";
+    loginButton.value = "Войти";
+    loginButton.name = "submitBtn";
+    loginButton.classList.add("login__btn");
+    form.appendChild(loginButton);
 
-    root.appendChild(div);
+    // let emailLabelWarning;
+    // if (warning) {
+    //     emailLabelWarning = document.createElement("Label");
+    //     emailLabelWarning.classList.add("warning");
+    //     emailLabelWarning.innerHTML = '<p>wrong data (login or password)</p>';
+    // }
+
+    // const submitBtn = document.createElement('Button');
+    // submitBtn.type = 'submit';
+    // submitBtn.classList.add("registerbtn");
+    // submitBtn.innerHTML = 'Войти';
+
+    // form.appendChild(emailLabel);
+    // form.appendChild(emailInput);
+    // form.appendChild(passwordLabel);
+    // form.appendChild(passwordInput);
+    // if (warning) {
+    //     form.appendChild(emailLabelWarning);
+    // }
+    // form.appendChild(submitBtn);
+
+    content.appendChild(div);
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
         const email = emailInput.value.trim();
-        const password1 = passwordInput.value;
-        const password2 = repeatePasswordInput.value;
+        const password = passwordInput.value;
 
         ajax(
             'POST',
-            '/signup',
-            { email, password1, password2 },
+            '/login',
+            { email, password },
             (status => {
                 if (status === 200) {
                     alert('All good');
@@ -109,19 +107,29 @@ export function signupPage(warning = 0) {
                     return;
                 }
                 if (status === 400) {
-                    alert('Invalid data bruh');
-                    signupPage(0);
+                    alert('Input ur data bruh');
+                    loginPage(false);
                     return;
                 }
                 if (status === 401) {
-                    signupPage(1);
-                    return;
-                }
-                if (status === 402) {
-                    signupPage(2);
+                    loginPage(true);
                     return;
                 }
             })
         )
     });
+    // if (warning != 0) {
+    //     form.appendChild(warningLabel); 
+    // }
+    // form.appendChild(terms);
+    // form.appendChild(submitBtn);
+
+    // root.appendChild(div);
+
+    // form.addEventListener('submit', (e) => {
+    //     e.preventDefault();
+
+    //     const email = emailInput.value.trim();
+    //     const password1 = passwordInput.value;
+    //     const password2 = repeatePasswordInput.value;
 }
