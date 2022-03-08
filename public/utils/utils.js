ec3d34d3e82fa61175e08e583bd98b4a9179893e
Kostich31 <k.v.kostinich@mail.ru>
2022-03-09 02:08:48 +0300

    Исправил ошибки

--- public/utils/utils.js
+++ public/utils/utils.js
@@ -1,7 +1,8 @@
-import { mainPage } from "../modules/mainPage";
-import { loginPage } from "../modules/loginPage";
-import { URL, OK } from "./consts";
-import { changeNavbarButton } from "../components/header/header";
+import { mainPage } from "../modules/mainPage.js";
+import { loginPage } from "../modules/loginPage.js";
+import { URL, OK } from "./consts.js";
+import { changeNavbarButton } from "../components/header/header.js";
+
 export function createElementFromHTML(html) {
   const temp = document.createElement('div');
   temp.innerHTML = html;
