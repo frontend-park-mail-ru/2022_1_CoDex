41fab0dad5efe42ec191322ca8e3e8e7ad15cd1a
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-03-08 22:05:58 +0300

    Grid movie

--- public/components/movie/movie.js
+++ public/components/movie/movie.js
@@ -1,11 +1,18 @@
 import {createElementFromHTML} from '../../utils/utils.js';
 
+function processDescription(description) {
+  if (description.length < 190) 
+    return description;
+  return description.slice(0, description.slice(0, 190).lastIndexOf(' ')) + "...";
+}
+
 /**
  * @param { Array } params Данные о фильме
  * @returns { HTMLElement } Карточка фильма, полученная по переданным данным.
  * @description Создаёт и возвращает карточку фильма, полученную по переданным данным.
  */
 export function createMovie(params) {
+  params.description = processDescription(params.description);
   const template = createElementFromHTML(movie(params));
   return template;
 }
