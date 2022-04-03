b35256ee076aa32c2c6aff40288c5d9e1f903843
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-04-03 18:05:27 +0300

    MovieModel filled

--- public/models/MovieModel.js
+++ public/models/MovieModel.js
@@ -1,1 +1,35 @@
-// TODO
+
+/**
+ * @description Класс модели страницы одного фильма.
+ */
+export class MovieModel {
+    /**
+     * @description Создаёт модель страницы одного фильма.
+     * @param { EventBus } eventBus Глобальная шина событий
+     */
+    constructor(eventBus) {
+        this.eventBus = eventBus;
+    }
+
+    getContent = (movie) => {
+        // TODO
+    }
+
+    sendReview = (inputsData = {}) => {
+        // TODO
+    }
+
+    sendRating = (movieID, rating) => {
+        // TODO
+    }
+
+    addCollection = (movieID, collectionID) => {
+        // TODO
+    }
+
+    createCollection = (collection) => {
+        // TODO
+    }
+
+    
+}
