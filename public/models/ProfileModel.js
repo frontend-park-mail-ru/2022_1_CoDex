258057da06e3e8d6f6501a99d32bdd214858de97
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-04-03 19:00:47 +0300

    ProfileModel filled

--- public/models/ProfileModel.js
+++ public/models/ProfileModel.js
@@ -1,1 +1,63 @@
-// TODO
+import { BaseModel } from "./BaseModel.js";
+
+/**
+ * @description Класс модели страницы профиля.
+ */
+ export class ProfileModel extends BaseModel {
+    /**
+     * @description Создаёт модель страницы одного фильма.
+     * @param { EventBus } eventBus Глобальная шина событий
+     */
+    constructor(eventBus) {
+        super(eventBus);
+        this.errorMessages = new Map();
+    }
+
+    getIDFromURL = (url) => {
+        // TODO
+    }
+
+    getContent = (URLData) => {
+        // TODO
+    }
+
+    changeProfile = async (inputsData={}, formData) => {
+        // TODO
+    }
+
+    changeAvatar = async (avatar) => {
+        // TODO
+    }
+    
+    getChangeContent = () => {
+        // TODO
+    }
+
+    initErrorMessages = () => {
+        // TODO
+    }
+
+    addError = () => {
+        // TODO
+    }
+
+    deleteError = () => {
+        // TODO
+    }
+
+    deleteAllErrors = () => {
+        // TODO
+    }
+
+    submitChanges = (inputsData) => {
+        // TODO
+    }
+
+    hasErrors = (inputsData) => {
+        // TODO
+    }
+
+    validateSingleInput = (inputName, inputValue) => {
+        // TODO ?
+    }
+}
