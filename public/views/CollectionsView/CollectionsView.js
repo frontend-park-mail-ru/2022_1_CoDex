4350076fdc070c65a62dacc9afa5f75201fc7dbc
Kostich31 <k.v.kostinich@mail.ru>
2022-04-03 15:58:50 +0300

    Добавил yml

--- public/views/CollectionsView/CollectionsView.js
+++ public/views/CollectionsView/CollectionsView.js
@@ -1,27 +1,27 @@
-import { BaseView } from "../BaseView/BaseView.js"
-import { events } from "../../consts/events.js";
-import collectionsContent from "../../components/collections/collections.pug";
+import {BaseView} from '../BaseView/BaseView.js';
+import {events} from '../../consts/events.js';
+import collectionsContent from '../../components/collections/collections.pug';
 
 /**
  * @description Класс представления страницы подборок
  */
- export class CollectionsView extends BaseView {
+export class CollectionsView extends BaseView {
   /**
    * @description Создаёт представление страницы подборок.
    * @param { EventBus } eventBus Глобальная шина событий
    * @param { Object } data Данные, необходимые для создания представления
    */
   constructor(eventBus, {data={}} = {}) {
-      super(eventBus, data);
+    super(eventBus, data);
   }
 
   /**
-   * @description Отправляет на глобальную шину событий событие отрисовки 
-   * контента страницы. 
+   * @description Отправляет на глобальную шину событий событие отрисовки
+   * контента страницы.
    */
   emitGetContent = () => {
-      this.eventBus.emit(events.collectionsPage.getContent);
-  }
+    this.eventBus.emit(events.collectionsPage.getContent);
+  };
 
   /**
    * @description Отрисовывает контент страницы подборок.
@@ -29,14 +29,14 @@ import collectionsContent from "../../components/collections/collections.pug";
    * массив подборок
    */
   renderContent = (data) => {
-      console.log(123);
-      const template = collectionsContent(data);
-      this.moviesData = data;
-      const content = document.querySelector(".content");
-      if (content) {
-          content.innerHTML = template;
-      } else {
-          this.eventBus.emit(events.app.errorPage);
-      }
-  }
+    console.log(123);
+    const template = collectionsContent(data);
+    this.moviesData = data;
+    const content = document.querySelector('.content');
+    if (content) {
+      content.innerHTML = template;
+    } else {
+      this.eventBus.emit(events.app.errorPage);
+    }
+  };
 }
