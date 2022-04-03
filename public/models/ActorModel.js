675de1d62799f742ace20cc9eac79f89a9c08577
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-04-03 18:24:07 +0300

    ActorModel filled

--- public/models/ActorModel.js
+++ public/models/ActorModel.js
@@ -1,1 +1,21 @@
-// TODO
+
+/**
+ * @description Класс модели страницы актёра.
+ */
+ export class MovieModel {
+    /**
+     * @description Создаёт модель страницы актёра.
+     * @param { EventBus } eventBus Глобальная шина событий
+     */
+    constructor(eventBus) {
+        this.eventBus = eventBus;
+    }
+
+    getContent = (actor) => {
+        // TODO
+    }
+
+    getJobContent = (actor) => {
+        // TODO
+    }
+}
