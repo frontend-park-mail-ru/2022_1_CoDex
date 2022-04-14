7b66fc65f1398cde2c523bf40d015f2bc7e8a0df
Kostich31 <k.v.kostinich@mail.ru>
2022-04-14 15:25:34 +0300

    Изменил урл

--- public/consts/urls.js
+++ public/consts/urls.js
@@ -2,7 +2,7 @@
 const localUrl = "http://localhost:3001";
 
 // eslint-disable-next-line no-unused-vars
-export const deployUrl = "https://localhost:3001";
+export const deployUrl = "https://park-akino:3001";
 //https://teamprojectkinopoisk.herokuapp.com
 
 export const currentUrl = deployUrl;
