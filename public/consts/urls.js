67de00bd07152d6e82cf2f25d3a85d753b5ad136
Konstantin <k.v.kostinich@mail.ru>
2022-04-19 21:44:50 +0300

    Update urls.js

--- public/consts/urls.js
+++ public/consts/urls.js
@@ -2,7 +2,7 @@
 const localUrl = "http://localhost:3001";
 
 // eslint-disable-next-line no-unused-vars
-export const deployUrl = "http://localhost:3000";
+export const deployUrl = "https://park-akino.ru";
 //https://teamprojectkinopoisk.herokuapp.com
 
 export const currentUrl = deployUrl;
