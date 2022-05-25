598ba010da09c60154394f18a12fe88b2c28a5cd
Kostich31 <k.v.kostinich@mail.ru>
2022-05-25 10:16:42 +0300

    change scripts firebase name

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.8.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.8.1/firebase-messaging-compat.js');
 
 function initializeApp() {
   const firebaseConfig = {
