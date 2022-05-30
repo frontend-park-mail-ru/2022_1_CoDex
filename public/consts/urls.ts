ce9b3fb6254315a19817cae7fab108c26e6adfdd
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-05-30 22:35:42 +0300

    Small fixes

--- public/consts/urls.ts
+++ public/consts/urls.ts
@@ -6,7 +6,7 @@ const localUrl = "http://localhost:3000";
 // eslint-disable-next-line no-unused-vars
 export const deployUrl = "https://park-akino.ru";
 
-export const currentUrl = localUrl;
+export const currentUrl = deployUrl;
 
 const version = "/api/v1";
 
